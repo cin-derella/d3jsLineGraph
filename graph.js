6d71eca2d4d3a751a4a6d2977e6881919d4b7abb
cin-derella <shirley_cinderella@outlook.com>
2019-10-26 20:59:08 +0800

    graph setup

--- graph.js
+++ graph.js
@@ -1,3 +1,18 @@
+const margin = {top:40,right:20,bottom:50,left:100};
+const graphWidh = 560-margin.left-margin.right;
+const graphHeight = 400-margin.top-margin.bottom;
+
+const svg =d3.select('.canvas')
+    .append('svg')
+    .attr('width',graphWidh+margin.left+margin.right)
+    .attr('height',graphHeight+margin.top+margin.bottom);
+
+const graph = svg.append('g')
+    .attr('width',graphWidh)
+    .attr('height',graphHeight)
+    .attr('transform',`translate(${margin.left},${margin.top})`);
+
+
 const update = (data)=>{
     console.log(data);
 }
